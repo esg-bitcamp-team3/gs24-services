21fae6195b6d717b108338da856c7abfb435325d
tjdus <dusml0277@gmail.com>
2025-05-19 11:34:08 +0900

    받아라

--- services/crawl-service/news/views/news.py
+++ services/crawl-service/news/views/news.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 import time
 import random
@@ -17,8 +18,9 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
+service = Service('/usr/bin/chromedriver')
 
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 class CompanyNewsView(APIView):
     def get(self, request):
