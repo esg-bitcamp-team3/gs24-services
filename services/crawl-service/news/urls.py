c30c09c3b520b750636a2886a396db6c45bda81b
Sangjin Jeong <sangjinjeong@Sangjinui-MacBookPro.local>
2025-05-21 15:43:15 +0900

    kafka 사용 안하는 것까진 끝

--- services/crawl-service/news/urls.py
+++ services/crawl-service/news/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from news.views.news import CompanyNewsView
+from news.views.final import FinalCompanyNewsView
 
 urlpatterns = [
   path('news', CompanyNewsView.as_view(), name='company-news'),
+  path('final', FinalCompanyNewsView.as_view(), name='company-news'),
 ]
